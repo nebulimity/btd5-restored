70225beacd8e17cb4d821db4bd10c1dd25337ed6
Koen Kruk <78868475+KoenKruk@users.noreply.github.com>
2025-12-09 16:58:45 -0500

    add play button

---
+++ scripts/button.js
@@ -0,0 +1,6 @@
+document.getElementById("load-game-btn").addEventListener("click", function () {
+  var container = document.getElementById("game-container");
+
+  container.innerHTML =
+    '<iframe src="game/BTD5 Restored.html" frameborder="0" width="800px" height="600px"></iframe>';
+});
