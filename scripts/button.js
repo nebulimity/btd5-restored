75d810a36843aae223c6c5d167601e969fa87a2a
nebulimity <koen.kruk@icloud.com>
2026-01-13 10:49:57 -0500

    disable blur and shadow in fullscreen

--- scripts/button.js
+++ scripts/button.js
@@ -1,6 +1,10 @@
 document.getElementById("load-game-btn").addEventListener("click", function () {
   var container = document.getElementById("game-container");
 
+  container.style.boxShadow = "none";
+  container.style.backgroundImage = "none";
+  container.style.borderRadius = "0px";
+
   container.innerHTML =
-    '<iframe src="game/BTD5 Restored.html" frameborder="0" width="800px" height="600px"></iframe>';
+    '<iframe src="game/BTD5 Restored.html" frameborder="0" width="800px" height="600px" allowfullscreen allow="fullscreen"></iframe>';
 });
---
+++ scripts/fullscreen-optimizations.js
@@ -0,0 +1,18 @@
+(function () {
+  function fsElement() {
+    return (
+      document.fullscreenElement ||
+      document.webkitFullscreenElement ||
+      document.msFullscreenElement ||
+      null
+    );
+  }
+  function onChange() {
+    const fs = fsElement();
+    if (fs && fs.tagName === "IFRAME") document.documentElement.classList.add("game-fullscreen");
+    else document.documentElement.classList.remove("game-fullscreen");
+  }
+  document.addEventListener("fullscreenchange", onChange);
+  document.addEventListener("webkitfullscreenchange", onChange);
+  window.addEventListener("load", onChange);
+})();
